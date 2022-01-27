8941f75cbe8ee543e38c436b91d687aea3b37955
ayat93a <alkayedayat93@gmail.com>
2022-01-27 22:31:36 +0200

    without css

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
-function employee (employeeID, fullName, department, level, /*ImageURL*/ ) {
-    this.employeeID = employeeID ;
-    this.fullName = fullName ;
-    this.department = department ;
-    this.level = level ;
-    this.salary = 0 ;
-    }
-    
-    const ghazi = new employee (1000 ,"Ghazi Samer", "Administration" ,"senior" );
-    const lana = new employee (1001, 'Lana Ali', 'Finance', 'senior') ;
-    const tamara = new employee (1002,	'Tamara Ayoub', 'Marketing' ,'senior');
-    const safi = new employee (1003,'Safi Walid','Administration',	'mid-senior');
-    const omar = new employee (1004,'Omar Zaid','Development',	'senior');
-    const rana = new employee (1005,'Rana Saleh','Development','junior');
-    const hadi = new employee (1006,'Hadi Ahmad','Finance','mid-senior');
-    
-    employee.prototype.payment = function () { 
-        if (this.level == "senior") {
-            let max = 2000 ;
-            let min = 1500 ;
-            this.salary = ((( Math.random () * (max-min) + min )) * .925) ; 
-            // net salary =  the salary * .925
-        }  else if (this.level == "mid-senior") {
-            let max = 1500;
-            let min = 2000;
-            this.salary = ((( Math.random () * (max-min) + min )) *.925 );
-        } else {
-            let max = 1000 ;
-            let min = 500;
-            this.salary =  ((( Math.random () * (max-min) + min )) *.925 );
-        }
-    }
-    
-    employee.prototype.render = function () {
-        `${this.fullName}.payment()`;
-    document.write(`${this.fullName} net sallary is ${this.salary} <br>`);
-    }
-    
-    ghazi.payment ();
-    lana.payment ();
-    tamara.payment ();
-    safi.payment ();
-    omar.payment ();
-    rana.payment ();
-    hadi.payment ();
-    ghazi.render () ;
-    lana.render();
-    tamara.render ();
-    safi.render();
-    omar.render ();
-    rana.render ();
-    hadi.render ();
+function employee( fullName, department, level, employeeID, ImageURL) {
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.salary = 0;
+    this.employeeID = 0;
+    this.ImageURL = 0
+}
+/*object*/
+const ghazi = new employee( "Ghazi Samer", "Administration", "senior",);
+const lana = new employee( 'Lana Ali', 'Finance', 'senior');
+const tamara = new employee('Tamara Ayoub', 'Marketing', 'senior');
+const safi = new employee('Safi Walid', 'Administration', 'mid-senior');
+const omar = new employee( 'Omar Zaid', 'Development', 'senior');
+const rana = new employee( 'Rana Saleh', 'Development', 'junior');
+const hadi = new employee( 'Hadi Ahmad', 'Finance', 'mid-senior');
+
+/*Uniqe unique four digits employee id number.*/
+employee.prototype.createId = function () {
+    this.employeeID = Math.floor(1000 + Math.random() * 9000);
+    return this.employeeID;
+}
+this.employeeID = [ghazi.createId(), lana.createId(), tamara.createId(),
+safi.createId(), omar.createId(), rana.createId(), hadi.createId()]
+ //console.log(this.employeeID);
+ 
+ //EventListener
+let employeeform = document.getElementById('employeeInfo');
+employeeform.addEventListener("submit", newEmployeeInfo);
+
+function newEmployeeInfo (event) {
+event.preventDefault() ;
+let fullName=event.target.fullName.value;
+let department=event.target.department.value;
+let level=event.target.level.value;
+let ImageURL=event.target.ImageUrl.value;
+return (fullName,department,level,ImageURL); 
+}
+//salary
+ employee.prototype.payment = function () {
+     if (this.level == "senior") {
+         let max = 2000 ;
+         let min = 1500 ;
+         this.salary = ((( Math.random () * (max-min) + min )) * .925) ; 
+         // net salary =  the salary * .925
+     }  else if (this.level == "mid-senior") {
+         let max = 1500;
+         let min = 2000;
+         this.salary = ((( Math.random () * (max-min) + min )) *.925 );
+     } else {
+         let max = 1000 ;
+         let min = 500;
+         this.salary =  ((( Math.random () * (max-min) + min )) *.925 );
+     }
+ }
+this.salary = [ghazi.payment (),lana.payment () ,tamara.payment () ,
+    safi.payment () ,omar.payment () ,rana.payment () ,hadi.payment ()]
+
+//console.log (ghazi)
+
+
+
+ /*
+ ghazi.payment ();
+ lana.payment ();
+ tamara.payment ();
+ safi.payment ();
+ omar.payment ();
+ rana.payment ();
+ hadi.payment ();
+
+
+ /*employee.prototype.render = function () {
+     `${this.fullName}.payment()`;
+ document.write(`${this.fullName} net sallary is ${this.salary} <br>`);
+ }*/
+ /*
+ ghazi.render () ;
+ lana.render();
+ tamara.render ();
+ safi.render();
+ omar.render ();
+ rana.render ();
+ hadi.render ();*/
