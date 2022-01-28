30e839bd641208a3153f59e4a68603f445939670
ayat93a <alkayedayat93@gmail.com>
2022-01-28 21:37:59 +0200

    09

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-function employee( fullName, department, level, employeeID, ImageURL) {
+employee.allEmployee = []
+function employee( fullName, department, level, employeeID, ImageUrl) {
     this.fullName = fullName;
     this.department = department;
     this.level = level;
     this.salary = 0;
     this.employeeID = 0;
-    this.ImageURL = 0
+    this.ImageUrl = 0;
+    employee.allEmployee.push(this);
 }
+
 /*object*/
-const ghazi = new employee( "Ghazi Samer", "Administration", "senior",);
+const ghazi = new employee( "Ghazi Samer", "Administration", "senior");
 const lana = new employee( 'Lana Ali', 'Finance', 'senior');
 const tamara = new employee('Tamara Ayoub', 'Marketing', 'senior');
 const safi = new employee('Safi Walid', 'Administration', 'mid-senior');
 const omar = new employee( 'Omar Zaid', 'Development', 'senior');
 const rana = new employee( 'Rana Saleh', 'Development', 'junior');
-const hadi = new employee( 'Hadi Ahmad', 'Finance', 'mid-senior');
+const hadi = new employee('Hadi Ahmad', 'Finance', 'mid-senior');
+
+
+//console.log (employee.allEmployee )
 
 /*Uniqe unique four digits employee id number.*/
 employee.prototype.createId = function () {
@@ -28,14 +34,26 @@ safi.createId(), omar.createId(), rana.createId(), hadi.createId()]
 let employeeform = document.getElementById('employeeInfo');
 employeeform.addEventListener("submit", newEmployeeInfo);
 
-function newEmployeeInfo (event) {
+/*function newEmployeeInfo (event) {
 event.preventDefault() ;
 let fullName=event.target.fullName.value;
 let department=event.target.department.value;
 let level=event.target.level.value;
-let ImageURL=event.target.ImageUrl.value;
-return (fullName,department,level,ImageURL); 
-}
+let ImageUrl=event.target.ImageUrl.value;
+return(fullName,department,level,ImageUrl); 
+}*/
+
+function newEmployeeInfo (event) {
+    event.preventDefault() ;
+    let fullName=event.target.fullName.value;
+    let department=event.target.department.value;
+    let level=event.target.level.value;
+    let ImageUrl=event.target.ImageUrl.value;
+    settingItems();
+   return (fullName,department,level,ImageUrl); 
+};
+
+//newEmployeeInfo
 //salary
  employee.prototype.payment = function () {
      if (this.level == "senior") {
@@ -57,27 +75,42 @@ this.salary = [ghazi.payment (),lana.payment () ,tamara.payment () ,
     safi.payment () ,omar.payment () ,rana.payment () ,hadi.payment ()]
 
 //console.log (ghazi)
- /*
+ 
  ghazi.payment ();
  lana.payment ();
  tamara.payment ();
  safi.payment ();
  omar.payment ();
  rana.payment ();
- hadi.payment ();*/
+ hadi.payment ();
+ 
+let salary = [ghazi.payment (),lana.payment () ,tamara.payment () ,
+        safi.payment () ,omar.payment () ,rana.payment () ,hadi.payment ()]  
 employee.prototype.render = function () {
      `${this.fullName}.payment()`;
  document.write(`${this.fullName} net sallary is ${this.salary} <br>`);
  }
- /*ghazi.render () ;
+ ghazi.render () ;
  lana.render();
  tamara.render ();
  safi.render();
  omar.render ();
  rana.render ();
- hadi.render ();*/
+ hadi.render ();
 
- let divElG=document.getElementById("ghazi")
- let pElG=document.createElement('p');
-divElG.appendChild(pElG);
-pElG.textContent = ghazi.render () ;
+function settingItems(){
+    let data =JSON.stringify(employee.allEmployee);
+    localStorage.setItem('employee', data);
+    }
+    settingItems();
+
+function gettingItem (){
+    let stringObj =  localStorage.getItem('employee');
+  //console.log(stringObj)
+     let parseObj = JSON.parse (stringObj);
+    if (parseObj !== null) {
+        employee.allEmployee = parseObj
+    }
+   //render();
+}
+gettingItem();
